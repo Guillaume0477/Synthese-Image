af5295a151f683dbb2133445cab71271e483e0ad
Guillaume0477 <guillaumeduret1@gmail.com>
2020-09-17 23:58:10 +0200

    with shader

--- tutos/tuto9_materials.glsl
+++ tutos/tuto9_materials.glsl
@@ -4,14 +4,17 @@
 
 #ifdef VERTEX_SHADER
 layout(location= 0) in vec3 position;
+layout(location= 1) in vec2 texcoord;
 layout(location= 2) in vec3 normal;
-layout(location= 4) in uint material;
+layout(location= 3) in uint material;
 
 uniform mat4 mvpMatrix;
 uniform mat4 mvMatrix;
 
 out vec3 vertex_position;
+out vec2 vertex_texcoord;
 out vec3 vertex_normal;
+
 flat out uint vertex_material;
 /* decoration flat : le varying est un entier, donc pas vraiment interpolable... il faut le declarer explicitement */
 
@@ -21,6 +24,7 @@ void main( )
     
     // position et normale dans le repere camera
     vertex_position= vec3(mvMatrix * vec4(position, 1));
+    vertex_texcoord= texcoord;
     vertex_normal= mat3(mvMatrix) * normal;
     // ... comme d'habitude
     
@@ -35,11 +39,15 @@ void main( )
 out vec4 fragment_color;
 
 in vec3 vertex_position;
+in vec2 vertex_texcoord;
 in vec3 vertex_normal;
 flat in uint vertex_material;	// !! decoration flat, le varying est marque explicitement comme non interpolable  !!
 
 #define MAX_MATERIALS 16
 uniform vec4 materials[MAX_MATERIALS];
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+
 
 void main( )
 {
@@ -48,8 +56,11 @@ void main( )
     float cos_theta= max(0, dot(n, l));
     
     // recupere la couleur de la matiere du triangle, en fonction de son indice.
-    vec4 color= materials[vertex_material];
-    fragment_color= color * cos_theta;
+    vec4 colorm= materials[vertex_material];
+    vec4 color0= texture(texture0, vertex_texcoord);
+    vec4 color1= texture(texture1, vertex_texcoord);
+
+    fragment_color= color0 * color1 * colorm * cos_theta;
 }
 
 #endif

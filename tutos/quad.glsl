addde08cd01b5f0ec31b0a0831d59de48221c66c
Guillaume0477 <guillaumeduret1@gmail.com>
2020-11-05 16:03:53 +0100

    rendu diféré

--- tutos/quad.glsl
+++ tutos/quad.glsl
@@ -29,14 +29,15 @@ out vec4 color;
 
 void main()
 {
+    vec2 uv_tex = (vec2(posi.x, posi.y)+1)/2;
 
-    vec3 pos = texelFetch(position_texture, ivec2((vec2(posi.x, posi.y)+1)/2), 0).xyz;//texture(position_texture, (vec2(posi.x, posi.y)+1)/2);
+    vec3 pos = texelFetch(position_texture, ivec2(uv_tex), 0).xyz;//texture(position_texture, (vec2(posi.x, posi.y)+1)/2);
     vec3 l= normalize(-pos);// la camera est la source de lumiere.
-    vec3 normal = texelFetch(normal_texture, ivec2(posi.x, posi.y), 0).xyz;// texture(normal_texture, (vec2(posi.x, posi.y)+1)/2);
-    vec3 n= normalize(normal);
-    float cos_theta= max(0, dot(n, l));
-    vec4 colorm = texture(material_texture, (vec2(posi.x, posi.y)+1)/2);
-	color = vec4(0.5-cos_theta,0.5-cos_theta,0.5-cos_theta,1);//*cos_theta;
+    vec4 normal = (texture(normal_texture, uv_tex))*2-1;//texelFetch(normal_texture, ivec2(posi.x, posi.y), 0).xyz;
+    vec4 n= normalize(normal);
+    float cos_theta= max(0, dot(n.xyz, l));
+    vec4 colorm = texture(material_texture, (uv_tex));
+	color = colorm*cos_theta;//vec4(0.5-cos_theta,0.5-cos_theta,0.5-cos_theta,1);//*cos_theta;
 
 };
 
